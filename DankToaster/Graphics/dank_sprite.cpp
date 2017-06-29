6ecdba8326f776c35afde1643734041039fd2e94
Jacob Lopez <jacob_m_lopez@berkeley.edu>
2017-06-29 14:32:59 -0700

    Fixed bugs from previous commit
    Shader class, sprite struct, vertex struct all work properly. Renders square on screen. approximately 1600 fps

--- DankToaster/Graphics/dank_sprite.cpp
+++ DankToaster/Graphics/dank_sprite.cpp
@@ -1,10 +1,17 @@
 #include "dank_sprite.h"
 
 dank_sprite::dank_sprite(float xcoord, float ycoord, float width, float height, dank_vec4 color) {
-	vertices[1] = { dank_vec3(xcoord, ycoord, 1), color };
-	vertices[2] = { dank_vec3(xcoord + width, ycoord, 1), color };
-	vertices[3] = { dank_vec3(xcoord + width, ycoord + height, 1), color };
-	vertices[4] = { dank_vec3(xcoord, ycoord + height, 1), color };
+	vertices[0].position = dank_vec3(xcoord, ycoord, 0);
+	vertices[0].color = color;
+
+	vertices[1].position = dank_vec3(xcoord + width, ycoord, 0);
+	vertices[1].color = color;
+
+	vertices[2].position = dank_vec3(xcoord + width, ycoord + height, 0);
+	vertices[2].color = color;
+	
+	vertices[3].position = dank_vec3(xcoord, ycoord + height, 0);
+	vertices[3].color = color;
 }
 dank_sprite::~dank_sprite() {
 }
--- DankToaster/main.cpp
+++ DankToaster/main.cpp
@@ -10,21 +10,20 @@ int main() {
 	dank_shader shader("Shaders/spriteShader.vert", "Shaders/spriteShader.frag");
 	shader.enable();
 	dank_sprite test(2, 2, 5, 5, dank_vec4(1, 1, 1, 1));
-	unsigned int indices[] = { 0,1,4,4,3,2 };
+	unsigned int indices[6] = { 0, 1, 3, 3, 1, 2 };
 	dank_mat4 projection = orthographic(0.0f, 16.0f, 0.0f, 9.0f, 0.0f, 2.0f);
 	shader.setUniformMat4("projection", projection);
-	GLuint VAO;
-	GLuint VBO;
-	GLuint EBO;
+	GLuint VAO, VBO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
 	glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, 4 * sizeof(float), test.vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(test.vertices), test.vertices, GL_STATIC_DRAW);
+
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(dank_vertex), 0);
